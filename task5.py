d06ae3a1f4d5a7d30b7bd934862c1616877068fc
Oleska1601 <118464712+Oleska1601@users.noreply.github.com>
2023-02-11 18:25:40 +0300

    Create task5.py

---
+++ task5.py
@@ -0,0 +1,96 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+
+# @app.route('/')
+# @app.route('/index')
+# def index():
+#     return "Привет, Яндекс!"
+#
+#
+# @app.route('/list_prof/<list>')
+# def form_sample(list):
+#     return render_template('/static/profession.html', list=list)
+
+
+@app.route('/astronaut_selection', methods=['POST', 'GET'])
+def astronaut_selection():
+    if request.method == 'GET':
+        return f'''<!doctype html>
+                        <html lang="en">
+                          <head>
+                            <meta charset="utf-8">
+                            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                            <link rel="stylesheet"
+                            href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+                            integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+                            crossorigin="anonymous">
+                            <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
+                            <title>Пример формы</title>
+                          </head>
+                          <body>
+                            <h1>Форма для регистрации в суперсекретной системе</h1>
+                            <div>
+                                <form class="login_form" method="post">
+                                <input type="text" class="form-control" id="name" placeholder="Введите имя" name="name">
+                                <input type="text" class="form-control" id="surname" placeholder="Введите фамилию" name="surname">
+                                    <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Введите адрес почты" name="email">
+                                    <div class="form-group">
+                                        <label for="classSelect">Какое у вас образование?</label>
+                                        <select class="form-control" id="classSelect" name="class">
+                                          <option>Начальное</option>
+                                          <option>Среднее</option>
+                                          <option>Общее</option>
+                                          <option>Высшее</option>
+                                        </select>
+                                     </div>
+                                     <div class="form-group">
+                                        <label for="form-check">Какие у вас есть профессии?</label>
+                                        {''.join(map(lambda x: f'<div class="form-check"><input class="form-check-input" type="radio" value="{x[0]}" name="profession" id="{x[0]}" checked><label class="form-check-label" for="male"></label></div>', ((0, 'пилот'), (1, 'метеоролог'), (2, 'врач'), (3, 'инженер-исследователь'), (4, 'инженер-строитель'), (5, 'инженер по радиационной защите'), (6, 'инженер по жизнеобеспечению'), (7, 'экзобиолог'))))} 
+                                     <div class="form-group">
+                                        <label for="form-check">Укажите пол</label>
+                                        <div class="form-check">
+                                          <input class="form-check-input" type="radio" name="sex" id="male" value="male" checked>
+                                          <label class="form-check-label" for="male">
+                                            Мужской
+                                          </label>
+                                        </div>
+                                        <div class="form-check">
+                                          <input class="form-check-input" type="radio" name="sex" id="female" value="female">
+                                          <label class="form-check-label" for="female">
+                                            Женский
+                                          </label>
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="about">Почему вы хотите принять участие в миссии?</label>
+                                        <textarea class="form-control" id="about" rows="3" name="about"></textarea>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="photo">Приложите фотографию</label>
+                                        <input type="file" class="form-control-file" id="photo" name="file">
+                                    </div>
+                                    <div class="form-group form-check">
+                                        <input type="checkbox" class="form-check-input" id="acceptRules" name="accept">
+                                        <label class="form-check-label" for="acceptRules">Готовы остаться на Марсе?</label>
+                                    </div>
+                                    <button type="submit" class="btn btn-primary">Отправить</button>
+                                </form>
+                            </div>
+                          </body>
+                        </html>'''
+    elif request.method == 'POST':
+        print(request.form['name'])
+        print(request.form['surname'])
+        print(request.form['email'])
+        print(request.form['class'])
+        print(request.form['file'])
+        print(request.form['about'])
+        print(request.form['accept'])
+        print(request.form['sex'])
+        return "Форма отправлена"
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
